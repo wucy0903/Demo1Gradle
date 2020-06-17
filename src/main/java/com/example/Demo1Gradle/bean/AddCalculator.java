ca982ab3e2661bfd491f95bb7ae07838c7f7bdb2
chenyi <wucy0903@gmail.com>
2020-06-17 14:47:17 +0800

    2020/06/17 Change Configuration-Bean to Component. Then, it don't need to use @Autowired to Inject.

--- src/main/java/com/example/Demo1Gradle/bean/AddCalculator.java
+++ src/main/java/com/example/Demo1Gradle/bean/AddCalculator.java
@@ -1,5 +1,8 @@
 package com.example.Demo1Gradle.bean;
 
+import org.springframework.stereotype.Component;
+
+@Component(value = "primary")
 public class AddCalculator  implements Calculator{
     @Override
     public int calculate(int a, int b) {
--- src/main/java/com/example/Demo1Gradle/bean/SubCalculator.java
+++ src/main/java/com/example/Demo1Gradle/bean/SubCalculator.java
@@ -1,5 +1,8 @@
 package com.example.Demo1Gradle.bean;
 
+import org.springframework.stereotype.Component;
+
+@Component(value = "backup")
 public class SubCalculator implements Calculator{
 
     @Override
